c64610d67015b86011639342ca1c872af3dcfedf
github username <github email address>
2020-10-08 14:31:16 -0500

    Filter function

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,13 @@ import Nav from "./Nav";
 import hogs from "../porkers_data";
 import HelloWorld from "./HelloWorld";
 import HogContainer from "./HogContainer";
+import Filter from "./Filter";
 
 class App extends Component {
 
   state = {
-    allHogs: hogs
+    allHogs: hogs,
+    filtered: []
   }
 
   hogObj = () => {
@@ -18,10 +20,24 @@ class App extends Component {
     return hogArray
   }
 
+  hogGreaseFilter = () => {
+    const hogGreasedArr = this.state.allHogs.filter(hog => hog.greased)
+      this.setState({
+        allHogs: hogGreasedArr
+      })
+   }
+
+  // renderGreased = () => {
+  //   this.setState({
+  //     allHogs: hogGreasedArr
+  //   })
+  // }
+
   render() {
     return (
       <div className="App">
         <Nav />
+        <Filter hogGreaseFilter ={this.hogGreaseFilter}/>
         {this.hogObj()}
       </div>
     );
